21d5f260e41a4d02718f6f3ec3e38d8a2282341c
Mike Riley <lvskiprof@cox.net>
2020-07-17 09:47:48 -0700

    Paddle class created again
    Basically everything working, except the Quit button on Gave Over scene always  loads Start Game scene again.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -12,42 +12,11 @@ public class Ball : MonoBehaviour
     void Start()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Paddle : MonoBehaviour
-{
-    static float    paddleWidthInUnits = 2f;
-    [SerializeField]
-    static float   screenWidthInUnits = 16f;            // Camera size of 6 in a 3:4 ratio screen
-    [SerializeField]
-    static float   minX = paddleWidthInUnits / 2;       // Paddle is 2 units wide, so width / 2
-    [SerializeField]
-    static float   maxX = screenWidthInUnits - minX;    // Since minX = paddleWidthInUnits / 2, simply subtract that from screenWidthInUnits
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
     }   // Start()
 
     // Update is called once per frame
     void Update()
     {
-        float   mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
-        transform.position = paddlePos;
+        
     }   // Update()
-}   // class Paddle
+}   // class Ball
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    static float    paddleWidthInUnits = 2f;
+    static float   screenWidthInUnits = 16f;     // Camera size of 6 in a 3:4 ratio screen
+    [SerializeField]
+    float   minX = paddleWidthInUnits / 2;       // Paddle is 2 units wide, so width / 2
+    [SerializeField]
+    float   maxX = screenWidthInUnits - (paddleWidthInUnits / 2);   // subtract that from screenWidthInUnits
+    [SerializeField]
+    float   test = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }   // Start()
+
+    // Update is called once per frame
+    void Update()
+    {
+        float   mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }   // Update()
+}   // class Paddle
